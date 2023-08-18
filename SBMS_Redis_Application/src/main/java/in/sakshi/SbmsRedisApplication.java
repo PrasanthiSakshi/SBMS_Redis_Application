81610b3f5648d0ed4257d18e8876cf4212632a4a
PrasanthiSakshi <135398157+PrasanthiSakshi@users.noreply.github.com>
2023-08-18 07:02:43 +0530

    Add files via upload

---
+++ SBMS_Redis_Application/src/main/java/in/sakshi/SbmsRedisApplication.java
@@ -0,0 +1,13 @@
+package in.sakshi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbmsRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbmsRedisApplication.class, args);
+	}
+
+}
